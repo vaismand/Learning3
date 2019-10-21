d181ec217d720cc70de08c6beabda44f55a4242c
David <omgemjey@gmail.com>
2019-10-21 11:33:31 +0200

    arrows

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    $('.carousel__wrapper').slick();
+    $('.carousel__wrapper').slick({
+        speed: 1000,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/chevron-right-solid.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
 });
