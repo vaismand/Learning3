75a2e05877cb14021b582dd61ad757febe332170
David <omgemjey@gmail.com>
2019-10-26 14:39:34 +0200

    jscript

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carousel__wrapper').slick({
         speed: 1000,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron-left-solid.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/chevron-right-solid.png"></button>',
         responsive: [
