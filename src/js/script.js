040d58b12d6c4beeba5028e481736b3fa02db38f
David <omgemjey@gmail.com>
2019-11-02 12:39:23 +0100

    mailer

--- src/js/script.js
+++ src/js/script.js
@@ -47,7 +47,8 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow'); 
         });
     });
-    function validateForms(form) {
+    
+    function validateForms(form){
         $(form).validate({
             rules: {
                 name: {
@@ -62,36 +63,38 @@ $(document).ready(function(){
             },
             messages: {
                 name: {
-                    required: "Пожалуйства, введите свое имя",
-                    minlength: jQuery.validator.format("Введите {0} символов")
-                },
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
                 phone: "Пожалуйста, введите свой номер телефона",
                 email: {
-                    required: "Пожалуйста, введите свою почту",
-                    email: "Неправильно введен адрес почты"
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
                 }
             }
         });
-    }
+    };
 
-    validateForms ('#consult-form');
+    validateForms('#consult-form');
     validateForms('#consult form');
     validateForms('#order form');
 
-    $('input [name=phone]').mask("+7 (999) 999-99-99");
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 
     $('form').submit(function(e) {
         e.preventDefault();
         $.ajax({
             type: "POST",
-            url: "mailer/smart.php",
+            url: "/mailer/smart.php",
             data: $(this).serialize()
         }).done(function() {
             $(this).find("input").val("");
-            $('#consult, #order').fadeOut();
+            $('#consultation, #order').fadeOut();
             $('.overlay, #thanks').fadeIn('slow');
+
             $('form').trigger('reset');
         });
         return false;
     });
+
 });
