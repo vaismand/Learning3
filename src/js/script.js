71c0ddbc383724df388251067cd76eb28f03aeef
David2 <omgemjey@gmail.com>
2019-11-02 17:21:58 +0100

    optimization

--- src/js/script.js
+++ src/js/script.js
@@ -108,10 +108,11 @@ $(document).ready(function(){
         }
     });
 
-    $("a[href^='#']").click(function(){
+    $("a[href=#up]").click(function(){
         var _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
     });
 
+    new WOW().init();
 });
